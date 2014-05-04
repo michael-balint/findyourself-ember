096a4c4eca5b81bebf0d1c5655e8dd1aaf99516e
Balint, Michael <michael.balint@accenture.com>
2014-05-03 21:33:31 -0700

    Removed 3rd party libs.

--- Brocfile.js
+++ Brocfile.js
@@ -13,10 +13,7 @@ var app = new EmberApp({
     'ember-data.js',
     'app-shims.js',
     'ember-resolver.js',
-    'ember-load-initializers.js',
-    'jquery.magnific-popup.js',
-    'raphael.js',
-    'kartograph.js/kartograph-0.8.2.js'
+    'ember-load-initializers.js'
   ],
 
   // AKA whitelisted modules
